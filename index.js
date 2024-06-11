489ad77d749278a7e9c6ab67d07958c25365ca33
MdForhadHossainBabu <developerforhad2@gmail.com>
2024-06-11 17:11:15 +0600

    added to verify and many other section

--- index.js
+++ index.js
@@ -41,27 +41,40 @@ async function run() {
     // jwt related apis
 
     app.post('/jwt', async (req, res) => {
-      const user = req.body;
+      const user = req.body;  
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
       res.send({token})
     })
+
+
 
     // verify token of middlewares
     const verifyToken = (req, res, next) => {
       console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({message: 'forbidden access'})
+        return res.status(401).send({message: 'unauthorized access'})
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
         if (err) {
-         return res.status(401).send({message: 'forbidden access'})
+         return res.status(401).send({ message: 'unauthorized access' });
         }
         req.decoded = decoded;
         next()
      })
     }
 
+    // verify admin 
+    const verifyAdmin = async(req, res, next)=>{
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin'
+      if (!isAdmin) {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+  next()
+    }
 
     // wish list insert in db
     app.post('/wishlist', async (req, res) => {
@@ -76,6 +89,30 @@ async function run() {
       const cursor = await wishlistCollection.find(query).toArray();
       res.send(cursor);
     });
+    // verify button for manage item list
+    app.patch('/wishlist/verify/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          roles: 'verify'
+        }
+      }
+      const result = await wishlistCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
+    // reject button for manage item list
+    app.patch('/wishlist/reject/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          roles: 'reject'
+        }
+      }
+      const result = await wishlistCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
 
     app.post('/description', async (req, res) => {
       const item = req.body;
@@ -141,11 +178,26 @@ async function run() {
       res.send(result);
     });
     //  get all users & checking all existing email get
-    app.get('/users', verifyToken, async (req, res) => {
+    app.get('/users',  async (req, res) => {
     
       const cursor = await userCollection.find().toArray();
       res.send(cursor);
     });
+
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) { 
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+    })
 
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
